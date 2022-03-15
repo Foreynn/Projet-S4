a8606a3ee2e172e120a17eb9cce7478243109d8f
kylak <gustavberloty2@gmail.com>
2022-03-15 15:17:20 +0100

    bug resolved

--- chemin.c
+++ chemin.c
@@ -20,30 +20,29 @@ int found_size;
 struct point {int x; int y;};
 struct point srt; // sortie
 
-int __get_chemin(struct point entry) {
+int __get_chemin(struct point entry, int beginning) {
     
     struct trncn *t = &(trncns[entry.x][entry.y]);
     t->parcourue = 1;
     
     if (srt.x == entry.x && srt.y == entry.y) {
     // if we've arrived at the exit
-        printf("found");
-        return 1;
+	return 1;
     }
 
     int dgr = t->dgr;
-    if (dgr == 1) {
+    if (dgr == 1 && !beginning) {
     // if dead end
         return 0;
     }
-    enum direction *accs = t->accs;
+    enum direction *access = t->accs;
     
     for(int i = 0; i < dgr; i++) {
         struct point new_entry = {
             .x = entry.x,
             .y = entry.y,
         };
-        switch(accs[i]) {
+        switch(access[i]) {
             case 0: new_entry.y--;
                 break;
             case 1: new_entry.y++;
@@ -56,19 +55,22 @@ int __get_chemin(struct point entry) {
 	if (new_entry.x < 0 || new_entry.y < 0) {
 		errx(0, "Erreur : direction de tronçon d'entrée."); 
 	}
-        if (trncns[new_entry.x][new_entry.y].parcourue == 0)
+        if (trncns[new_entry.x][new_entry.y].parcourue == 0) {
             // if the adjacent section has never been visited
-            if (__get_chemin(new_entry) == 1) {
+	    beginning = 0;
+            if (__get_chemin(new_entry, beginning) == 1) {
             // if we are on the way to the exit
             // we add the section to chemin_trouve
                 chemin_trouve[SIZE-1 - found_size] = &(trncns[new_entry.x][new_entry.y]);
-                found_size++;
+		found_size++;
                 return 1;
             }
+	}
     }
 
     // if we have not returned 1
     // then we return 0
+    beginning = 0;
     return 0;
 }
 
@@ -77,7 +79,13 @@ int __get_chemin(struct point entry) {
 
 void get_chemin(struct point entry) {
     found_size = 0;
-    __get_chemin(entry);
+    int beginning = 1;
+    __get_chemin(entry, beginning);
+    if (found_size > 0) {
+	// On ajouter la case de départ.
+	chemin_trouve[SIZE - 1 - found_size] = &(trncns[entry.x][entry.y]);
+    	found_size++;
+    }
     // struct trncn **tmp = malloc(found_size * sizeof(struct trncn *));
     //memcpy(tmp, chemin_trouve + SIZE-1-found_size, found_size);
     //free(chemin_trouve);
--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include "chemin.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main() {
 
@@ -9,143 +10,144 @@ int main() {
    
     SIZE = 15;
 
-    trncns = malloc(sizeof(struct trncn *) * 5);
-    for(int i = 0; i < 5; i++) {
-        trncns[i] = malloc(sizeof(struct trncn) * 3);
+    trncns = malloc(sizeof(struct trncn *) * 3);
+    for(int i = 0; i < 3; i++) {
+        trncns[i] = malloc(sizeof(struct trncn) * 5);
     }
-
+    
     struct trncn c0 = {
         .dgr = 1,
-        .accs = malloc(sizeof(enum direction)),
+        .accs = malloc(sizeof(int)),
         .parcourue = 0
     };
     c0.accs[0] = RIGHT;
     trncns[0][0] = c0;
     
+    struct trncn c3 = {
+        .dgr = 1,
+        .accs = malloc(sizeof(int)),
+        .parcourue = 0
+    };
+    c3.accs[0] = DOWN;
+    trncns[0][1] = c3;
+    
+    struct trncn c6 = {
+        .dgr = 2,
+        .accs = malloc(sizeof(int) * 3),
+        .parcourue = 0
+    };
+    c6.accs[0] = UP;
+    c6.accs[1] = RIGHT;
+    c6.accs[2] = DOWN;
+    trncns[0][2] = c6;
+    
+    struct trncn c9 = {
+        .dgr = 3,
+        .accs = malloc(sizeof(int) * 3),
+        .parcourue = 0
+    };
+    c9.accs[0] = UP;
+    c9.accs[1] = RIGHT;
+    c9.accs[2] = DOWN;
+    trncns[0][3] = c9;
+    
+    struct trncn c12 = {
+        .dgr = 1,
+        .accs = malloc(sizeof(int)),
+        .parcourue = 0
+    };
+    c12.accs[0] = UP;
+    trncns[0][4] = c12;
+    
     struct trncn c1 = {
         .dgr = 2,
-        .accs = malloc(sizeof(enum direction) * 2),
+        .accs = malloc(sizeof(int) * 2),
         .parcourue = 0
     };
     c1.accs[0] = LEFT;
     c1.accs[1] = DOWN;
-    trncns[0][1] = c1;
-    
-    struct trncn c2 = {
-        .dgr = 1,
-        .accs = malloc(sizeof(enum direction)),
-        .parcourue = 0
-    };
-    c2.accs[0] = DOWN;
-    trncns[0][2] = c2;
-    
-    struct trncn c3 = {
-        .dgr = 1,
-        .accs = malloc(sizeof(enum direction)),
-        .parcourue = 0
-    };
-    c3.accs[0] = DOWN;
-    trncns[1][0] = c3;
+    trncns[1][0] = c1;
     
     struct trncn c4 = {
         .dgr = 2,
-        .accs = malloc(sizeof(enum direction) * 2),
+        .accs = malloc(sizeof(int) * 2),
         .parcourue = 0
     };
     c4.accs[0] = UP;
     c4.accs[1] = RIGHT;
     trncns[1][1] = c4;
     
+    struct trncn c7 = {
+        .dgr = 1,
+        .accs = malloc(sizeof(int)),
+        .parcourue = 0
+    };
+    c7.accs[0] = LEFT;
+    trncns[1][2] = c7;
+    
+    struct trncn c10 = {
+        .dgr = 2,
+        .accs = malloc(sizeof(int) * 2),
+        .parcourue = 0
+    };
+    c10.accs[0] = LEFT;
+    c10.accs[1] = RIGHT;
+    trncns[1][3] = c10;
+    
+    struct trncn c13 = {
+        .dgr = 1,
+        .accs = malloc(sizeof(int)),
+        .parcourue = 0
+    };
+    c13.accs[0] = RIGHT;
+    trncns[1][4] = c13;
+    
+    struct trncn c2 = {
+        .dgr = 1,
+        .accs = malloc(sizeof(int)),
+        .parcourue = 0
+    };
+    c2.accs[0] = DOWN;
+    trncns[2][0] = c2;
+    
     struct trncn c5 = {
         .dgr = 3,
-        .accs = malloc(sizeof(enum direction) * 3),
+        .accs = malloc(sizeof(int) * 3),
 
         .parcourue = 0
     };
     c5.accs[0] = UP;
     c5.accs[1] = LEFT;
     c5.accs[2] = DOWN;
-    trncns[1][2] = c5;
-    
-    struct trncn c6 = {
-        .dgr = 2,
-        .accs = malloc(sizeof(enum direction) * 2),
-        .parcourue = 0
-    };
-    c6.accs[0] = UP;
-    c6.accs[1] = RIGHT;
-    trncns[2][0] = c6;
-    
-    struct trncn c7 = {
-        .dgr = 1,
-        .accs = malloc(sizeof(enum direction)),
-        .parcourue = 0
-    };
-    c7.accs[0] = LEFT;
-    trncns[2][1] = c7;
+    trncns[2][1] = c5;
     
     struct trncn c8 = {
         .dgr = 2,
-        .accs = malloc(sizeof(enum direction) * 2),
+        .accs = malloc(sizeof(int) * 2),
         .parcourue = 0
     };
     c8.accs[0] = UP;
     c8.accs[1] = DOWN;
     trncns[2][2] = c8;
     
-    struct trncn c9 = {
-        .dgr = 3,
-        .accs = malloc(sizeof(enum direction) * 3),
-        .parcourue = 0
-    };
-    c9.accs[0] = UP;
-    c9.accs[1] = RIGHT;
-    c9.accs[2] = DOWN;
-    trncns[3][0] = c9;
-    
-    struct trncn c10 = {
-        .dgr = 2,
-        .accs = malloc(sizeof(enum direction) * 2),
-        .parcourue = 0
-    };
-    c10.accs[0] = LEFT;
-    c10.accs[1] = RIGHT;
-    trncns[3][1] = c10;
-    
     struct trncn c11 = {
         .dgr = 3,
-        .accs = malloc(sizeof(enum direction) * 3),
+        .accs = malloc(sizeof(int) * 3),
         .parcourue = 0
     };
     c11.accs[0] = LEFT;
     c11.accs[1] = UP;
     c11.accs[2] = DOWN;
-    trncns[3][2] = c11;
-    
-    struct trncn c12 = {
-        .dgr = 1,
-        .accs = malloc(sizeof(enum direction)),
-        .parcourue = 0
-    };
-    c12.accs[0] = UP;
-    trncns[4][0] = c12;
-    
-    struct trncn c13 = {
-        .dgr = 1,
-        .accs = malloc(sizeof(enum direction)),
-        .parcourue = 0
-    };
-    c13.accs[0] = RIGHT;
-    trncns[4][1] = c13;
+    trncns[2][3] = c11;
     
     struct trncn c14 = {
         .dgr = 2,
-        .accs = malloc(sizeof(enum direction) * 2),
+        .accs = malloc(sizeof(int) * 2),
         .parcourue = 0
     };
     c14.accs[0] = UP;
     c14.accs[1] = LEFT;
-    trncns[4][2] = c14;
+    trncns[2][4] = c14;
 
     // We create the exit of our maze test.
     srt.x = 0;
@@ -155,21 +157,21 @@ int main() {
 
     // Where to stock the solution of the maze.
     chemin_trouve = malloc(sizeof(struct trncn *) * 15);
-    
+    memset(chemin_trouve, 0, sizeof(struct trncn *));
+
     // We choose an entry point.
     struct point entry = {.x = 0, .y = 0}; 
 
     // We run our solver.
     get_chemin(entry);
 
-    printf("Solution :");
-    printf("%i", found_size);
-    for(int s = 0; s <found_size; s++) {
+    printf("Solution: \n");
+    // printf("%i", found_size);
+    for(int s = 0; s < SIZE; s++) {
         for(int i = 0; i < 3; i++) {
             for(int j = 0; j < 5; j++) {
                 if (chemin_trouve[s] == &trncns[i][j]) {
-                    printf("case[%i][%i]", i, j);
-                    i = 3;
+                    printf("\t · case[%i][%i]\n", i, j);
                     break;
                 }
             }
