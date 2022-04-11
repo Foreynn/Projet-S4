c05f0fcd00c79ceb6b20e80f163f9af6201f92b0
Julien Auzanneau <julien.auzanneau@epita.fr>
2022-04-11 16:00:26 +0200

    interface pqs finie

---
+++ GUI/main.c
@@ -0,0 +1,25 @@
+#include <gtk/gtk.h>
+
+// Main function.
+int main (int argc, char *argv[])
+{
+    // Initializes GTK.
+    gtk_init(NULL, NULL);
+
+    // Loads the UI description and builds the UI.
+    // (Exits if an error occurs.)
+    GtkBuilder* builder = gtk_builder_new();
+    GError* error = NULL;
+    if (gtk_builder_add_from_file(builder, "main.glade", &error) == 0)
+    {
+        g_printerr("Error loading file: %s\n", error->message);
+        g_clear_error(&error);
+        return 1;
+    }
+
+    // Runs the main loop.
+    gtk_main();
+
+    // Exits.
+    return 0;
+}
